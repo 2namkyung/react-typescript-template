ca9d351513c7fe54caf5d46a44f64f60ec472be2
2namkyung <iddbxk@daum.net>
2022-01-21 00:09:53 +0900

    react typescript template init

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const NodePolyfillplugin = require('node-polyfill-webpack-plugin')
+
+module.exports = {
+	entry: path.join(__dirname, 'src'),
+    resolve:{
+        extensions:['.ts', '.tsx', '.js', '.jsx']
+    },
+
+    output:{
+        path: path.join(__dirname, 'dist'),
+        publicPath:'/',
+        filename: 'index_bundle.js',
+        clean: true
+    },
+
+    module:{
+        rules:[
+            {
+                test:/\.(ts|tsx)$/,
+                exclude:/node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test:/\.s?css$/,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+
+        new CopyPlugin({
+            patterns:[{from:'./public/static'}]
+        }),
+
+        new NodePolyfillplugin()
+    ],
+
+    devServer:{
+        historyApiFallback: true,
+        static: {directory: path.resolve(__dirname, 'dist')}
+    }
+};
